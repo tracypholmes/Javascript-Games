86393a07fd945e2ba3d15bc38bff1837c1ba11ca
Tracy Holmes <tracypholmes@users.noreply.github.com>
2016-05-09 18:03:02 -0500

    Create LearningIfwithScript.js

---
+++ LearningIfwithScript.js
@@ -0,0 +1,24 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<script type="text/javascript">
+	if (11 > 1) {
+		alert ("Good job!");
+	}
+	if (13 < 3) {
+		alert ("Wait, what?");
+	}
+	if ( prompt ("Are you happy or sad") === "sad" ){
+		console.log("Don't worry, everything will be fine...");
+	}
+	if (prompt ("What is the secret number?") !== 3 ){
+		alert ("WARNING: THIS MESSAGE WILL SELF DESTRUCT!!!!");
+	}
+</script>
+</body>
+
+
+</html>
