e8a691c1313534bfebb86f0f17a2065bf44985b0
Tracy Holmes <tracypholmes@users.noreply.github.com>
2016-05-09 18:20:51 -0500

    Update RockPaperScissors.js
    added missing close curly braces; updated alerts.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -2,44 +2,44 @@ var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 
 if (computerChoice < 0.34) {
-	computerChoice = "rock";
+  computerChoice = "rock";
 } 
 else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
+  computerChoice = "paper";
 } 
 else {
-	computerChoice = "scissors";
+  computerChoice = "scissors";
 } console.log("Computer: " + computerChoice);
 
 
 
 
 
-if (userChoice === conputerChoice) {
+if (userChoice === computerChoice) {
     alert ("The result is a tie!");
   } 
   
 else if (userChoice === "rock"){
-  if (conputerChoice === "scissors"){
+  if (computerChoice === "scissors"){
      alert ("rock wins");
   }
   else {
       alert ("paper wins");
       }
 if (userChoice === "paper"){
-    if (conputerChoice ==="rock"){
+    if (computerChoice ==="rock"){
         alert ("paper wins");
   }
   else {
       alert ("scissors wins");
       }
 if (userChoice === "scissors"){
-    if (conputerChoice ==="rock"){
+    if (computerChoice ==="rock"){
         alert ("rock wins");
   }
-    if (conputerChoice ==="paper"){
+    if (computerChoice ==="paper"){
         alert ("scissors wins");
  } else {
       alert ("scissors wins");
       }
-  }
+  }}}
